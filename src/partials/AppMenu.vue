15c99065e2468323a1c3323e9ded1aeeb8957468
Altbeg <shmuelbisberg.acc@gmail.com>
2024-03-10 01:39:08 +0200

    remove hops count calculations from links

--- src/partials/AppMenu.vue
+++ src/partials/AppMenu.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { ref, computed } from "vue";
-import { RouterLink } from "vue-router";
-import { useRoute } from "vue-router";
+import { RouterLink, useRouter } from "vue-router";
 import routes from "../router";
 
-const router = useRoute();
+const router = useRouter();
 
 const hops = ref(0);
 const maxHops = ref(0);
+
+router.afterEach((to, from) => {
+  if (from?.path !== to?.path) {
+    maxHops.value += 1;
+    hops.value += 1;
+  }
+});
 
 const hasNext = computed(() => hops.value < maxHops.value);
 const hasPrevious = computed(() => hops.value !== 0);
@@ -18,7 +24,7 @@ const hasPrevious = computed(() => hops.value !== 0);
     <nav class="flex text-center divide-x-2 shadow-lg mb-2">
       <button
         :disabled="!hasPrevious || undefined"
-        @click=" hops -= 1; $router.go(-1);"
+        @click=" hops -= 2; maxHops -= 1; $router.go(-1);"
         class="w-8 disabled:opacity-40 disabled:hover:bg-primary-200 bg-primary-200 font-semibold py-1 text-white hover:bg-primary-100 transform duration-200"
         title="Go back"
         type="button"
@@ -29,15 +35,14 @@ const hasPrevious = computed(() => hops.value !== 0);
         v-for="route in routes"
         :key="route.path"
         :to="route.path"
-        :data-current-page="router.name === route.name ? true : undefined"
-        @click="maxHops += 1; hops += 1;"
+        :data-current-page="router.currentRoute.value.name === route.name ? true : undefined"
         class="flex-1 capitalize data-[current-page]:cursor-default hover:data-[current-page]:bg-secondary-100 data-[current-page]:bg-secondary-100 bg-primary-200 font-semibold py-1 text-white hover:bg-primary-100 transform duration-200"
       >
         {{ route.meta?.displayName || route.name }}
       </router-link>
       <button
         :disabled="!hasNext || undefined"      
-        @click="hops += 1; $router.go(1);"
+        @click="maxHops -= 1; $router.go(1);"
         class="w-8 disabled:opacity-40 disabled:hover:bg-primary-200 bg-primary-200 font-semibold py-1 text-white hover:bg-primary-100 transform duration-200"
         title="Go forward"
         type="button"
