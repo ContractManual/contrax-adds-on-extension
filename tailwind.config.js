58cc63e3b18ef00d50d93f1d791a6eed3a60481e
Altberg <shmuelbisberg@gmail.com>
2024-03-09 22:52:04 +0200

    add placeholder palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#0a9396',
+          200: '#005f73',
+          300: '#001219',
+        },
+        secondary: {
+          100: '#ee9b00',
+          200: '#ca6702',
+          300: '#bb3e03',
+        },
+        error: '#ae2012',
+      }
+    },
   },
   plugins: [require('tailwindcss-animated')],
 };
