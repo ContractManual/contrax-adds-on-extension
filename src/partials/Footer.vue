fded94168da7a5624717a3ae0cf3325a98581ea2
Altbeg <shmuelbisberg.acc@gmail.com>
2024-03-10 01:06:15 +0200

    start footer style & content

--- src/partials/Footer.vue
+++ src/partials/Footer.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <footer class="bg-gray-900 text-white py-4">
+  <footer class="bg-primary-300 text-white py-4">
     <div class="container mx-auto px-4">
-      <p class="text-center">This is the footer content.</p>
+      <p class="text-center">&copy; Contrax co. {{ new Date().getFullYear() }}</p>
     </div>
   </footer>
 </template>
