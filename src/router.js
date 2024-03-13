13973fb48ac8a8bd842ee0bb44524666c4bb203b
robottttttttttttttttt <therobot.zee@gmail.com>
2024-03-13 23:34:48 +0200

    Fix typo

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
 export default [
-  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'heem' } },
+  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'home' } },
   { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
 ];
