a80d8264f9ab688e2b4c9e068c06f72e306687f8
robottttttttttttttttt <therobot.zee@gmail.com>
2024-03-13 23:24:39 +0200

    change displayname home to heem

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
 export default [
-  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'myHome' } },
+  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'heem' } },
   { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
 ];
