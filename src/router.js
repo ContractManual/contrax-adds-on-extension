10268aa7554e854e191c2e098ed70dbf281d1d84
Altbeg <shmuelbisberg.acc@gmail.com>
2024-03-13 22:59:05 +0200

    duplicate about rout

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 export default [
   { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'home' } },
   { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
+  { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
 ];
