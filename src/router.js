1fc6459326809b799ceecbc787c126fb931386f9
robottttttttttttttttt <therobot.zee@gmail.com>
2024-03-13 22:57:10 +0200

    Change home display name

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
 export default [
-  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'home' } },
+  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'myHome' } },
   { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
 ];
