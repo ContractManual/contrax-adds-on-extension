99e9b83784b988150f188c4f329143c906090711
Altbeg <shmuelbisberg.acc@gmail.com>
2024-03-13 22:06:49 +0200

    install shadcn ui, add configs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+import path from "path";
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { viteSingleFile } from "vite-plugin-singlefile";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue(), viteSingleFile()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
