178ba702c8e7e8136485a61d9dbd1170807c09f6
Altbeg <shmuelbisberg.acc@gmail.com>
2024-05-03 15:24:44 +0300

    add docs for shadcn and routes conf.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 use [tailwindcss](https://tailwindcss.com/) for styling.
 use [tailwindcss animated](https://www.tailwindcss-animated.com/) for animations.
 see our style system on [figma](https://www.figma.com/file/XS7FAOVrbjl8wnpWmVj0Mt/assets?type=design&t=He9VjZHI1DICBRiZ-6).
+
+### Component library [shadcn-vue](https://www.shadcn-vue.com/)
+
+
--- src/router.js
+++ src/router.js
@@ -1,3 +1,14 @@
+// Docs - Route manual configuration
+
+// keep the following format: page.sub-page.sub-sub-page
+// meaning: page -> sub-page -> sub-sub-page -> ... match the pat5h /page/sub-page/sub-sub-page
+// for each page, add the fields `path`, `component`, `name`, `meta.displayName` 
+
+// `path` is the url path
+// `component` is the component to be rendered
+// `name` is the name of the route
+// `meta.displayName` is the name to be displayed in the sidebar
+
 export default [
   { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'home' } },
   { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
