f9d8d42fb171a51d88dc5cf809b56f9e8209f7e5
Altbeg <shmuelbisberg.acc@gmail.com>
2024-05-06 18:32:15 +0300

    fill required info for routes headers

--- src/router.js
+++ src/router.js
@@ -8,10 +8,11 @@
 // `component` is the component to be rendered
 // `name` is the name of the route
 // `meta.displayName` is the name to be displayed in the sidebar
+// `meta.description` is the description to be displayed under the displayName
+// `meta.icon` is the icon to be displayed in the sidebar and in the name, see https://lucide.dev/guide/packages/lucide-vue-next
 
 export default [
-  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'home' } },
-  { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
-  { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About' } },
-  { path: '/setup', component: () => import('./pages/Setup.vue'), name: 'setup', meta: { displayName: 'Setup' } },
+  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'Home', description: "Welcome to the Home Page", icon: "Home" } },
+  { path: '/setup', component: () => import('./pages/Setup.vue'), name: 'setup', meta: { displayName: 'Setup', description: "Fill out the form below to analyze your document.", icon: "UserCog" } },
+  { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About', icon: "TestTubeDiagonal", } },
 ];
