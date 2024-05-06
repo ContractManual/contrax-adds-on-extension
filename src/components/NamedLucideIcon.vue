e39b7b3c52aa4211af3a07edc6e0e2a8f552ddc1
Altbeg <shmuelbisberg.acc@gmail.com>
2024-05-06 18:29:46 +0300

    featAdd NamedLucideIcon component for displaying Lucide icons with a given name

---
+++ src/components/NamedLucideIcon.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed, defineProps } from "vue";
+import * as icons from "lucide-vue-next";
+
+const props = defineProps({
+  icon: { type: String, required: false },
+  class: { type: String, required: false },
+});
+
+const icon = computed(() => icons[props.icon]);
+</script>
+
+<template>
+  <component v-if="icon" :is="icon" :class="props.class" />
+</template>
