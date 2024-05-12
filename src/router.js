9b48d00804c28067c6087f0cfb86ee41c64b4a82
Altbeg <shmuelbisberg.acc@gmail.com>
2024-05-12 18:40:57 +0300

    Add all app's routes

--- src/router.js
+++ src/router.js
@@ -14,5 +14,9 @@
 export default [
   { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'Home', description: "Welcome to the Home Page", icon: "Home" } },
   { path: '/setup', component: () => import('./pages/Setup.vue'), name: 'setup', meta: { displayName: 'Setup', description: "Fill out the form below to analyze your document.", icon: "UserCog" } },
-  { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About', icon: "TestTubeDiagonal", } },
+  { path: '/contract-analysis', component: () => import('./pages/ContractAnalysis.vue'), name: 'contract-analysis', meta: { displayName: 'Contract Analysis', description: "Analyze your document for Fairness, Onerousness, & Enforceability", icon: "FileBarChart2" } },
+  { path: '/legal-text', component: () => import('./pages/LegalText.vue'), name: 'legal-text', meta: { displayName: 'Legal Text', description: "", icon: "Gavel" } },
+  { path: '/basic-review', component: () => import('./pages/BasicReview.vue'), name: 'basic-review', meta: { displayName: 'Basic Review', description: "", icon: "Rabbit" } },
+  { path: '/in-depth-analysis', component: () => import('./pages/InDepthAnalysis.vue'), name: 'in-depth-analysis', meta: { displayName: 'In Depth Analysis', description: "", icon: "Search" } },
+  // { path: '/about', component: () => import('./pages/About.vue'), name: 'about', meta: { displayName: 'About', icon: "TestTubeDiagonal", } },
 ];
