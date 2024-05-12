d51fad97496ad9ae9b4501e15a30e6da7bc9f927
Altbeg <shmuelbisberg.acc@gmail.com>
2024-05-12 19:01:02 +0300

    Add links to futures on Contract Analysis page

--- src/pages/ContractAnalysis.vue
+++ src/pages/ContractAnalysis.vue
@@ -1,10 +1,26 @@
 <script setup>
+import routes from "../router";
+import { useRouter } from "vue-router";
+import { Button } from "@/components/ui/button";
+import NamedLucideIcon from "@/components/NamedLucideIcon.vue";
 
+const routesToExclude = ["home", "contract-analysis"];
+const routesToDisplay = routes.filter((route) => !routesToExclude.includes(route.name));
+
+const router = useRouter();
 </script>
 
 <template>
-  <div>
-    <h1>About Page</h1>
-    <p>Welcome to the About page!</p>
+  <div class="grid grid-cols-2 gap-2 px-1">
+    <Button
+      v-for="route in routesToDisplay"
+      :key="route.name"
+      @click="() => router.push({ name: route.name })"
+      variant="secondary"
+      class="flex items-center justify-center flex-col gap-2 h-fit shadow-md p-3"
+    >
+      <NamedLucideIcon :icon="route.meta.icon" class="size-6 shrink-0" />
+      <span class="shrink-0">{{ route.meta.displayName }}</span>
+    </Button>
   </div>
 </template>
