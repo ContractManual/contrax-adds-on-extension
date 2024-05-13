67b6e61cdaded420e349004e8d9d714c24731b30
Altbeg <shmuelbisberg.acc@gmail.com>
2024-05-13 12:18:30 +0300

    remove icon from headline

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { useRouter } from "vue-router";
 import AppMenu from "./partials/AppMenu.vue";
 import Footer from "./partials/Footer.vue";
-import NamedLucideIcon from "@/components/NamedLucideIcon.vue";
 import Toaster from "@/components/ui/toast/Toaster.vue";
 
 const { currentRoute } = useRouter();
@@ -27,7 +26,6 @@ const { currentRoute } = useRouter();
             class="scroll-m-20 text-2xl font-semibold tracking-tight flex items-center justify-center gap-3"
           >
             <span>{{ currentRoute.meta.displayName }}</span>
-            <NamedLucideIcon :icon="currentRoute.meta.icon" class="inline-block size-7" />
           </h3>
           <p class="text-slate-500 text-sm">
             {{ currentRoute.meta.description }}
