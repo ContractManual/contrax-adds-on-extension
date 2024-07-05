1806e4a38913ff50208a07ced0794510f1f079ca
Altbeg <shmuelbisberg.acc@gmail.com>
2024-07-05 17:07:07 +0300

    navigate acording to doc configuration

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,26 @@
 <script setup>
 import Button from "@/components/ui/button/Button.vue";
 import { useRouter } from "vue-router";
+import { docStore } from "../lib/doc-store";
 
 const router = useRouter();
+const { getDoc, isConfigured } = docStore;
+
+
+function getStarted() {
+  getDoc();
+  if (isConfigured()) {
+    router.push({ name: "contract-analysis" });
+  } else {
+    router.push({ name: "setup" });
+  }
+}
+
+
 </script>
 
 <template>
   <div class="home">
-    <button
-      type="button"
-      @click="
-        () =>
-          toast({
-            title: 'Hello, World!',
-            description: 'This is a toast message.',
-            
-          })
-      "
-    >
-  </button>
-  <Button variant="secondary" class="shadow-md mx-auto block" @click="() => router.push({ name: 'setup' })">Get Started</Button>
+    <Button variant="secondary" class="shadow-md mx-auto block" @click="getStarted">Get Started</Button>
   </div>
 </template>
