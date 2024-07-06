5857c0998edc6f3b92200c609e8cb3495332c33e
Altbeg <shmuelbisberg.acc@gmail.com>
2024-07-07 00:04:16 +0300

    show skelton and  make request `onMounted`

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,41 @@
 <script setup>
-import Button from "@/components/ui/button/Button.vue";
+import { Skeleton } from "@/components/ui/skeleton";
 import { docStore } from "../lib/doc-store";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 
 const { getDoc } = docStore;
+const router = useRouter();
 
-function getStarted() {
-  getDoc();
-}
-
+onMounted(() => {
+  getDoc(
+    (doc) => {
+      if (doc?.configured) {
+        router.push({ name: "contract-analysis" });
+      } else {
+        router.push({ name: "setup" });
+      }
+    },
+    (error) => console.error(error)
+  );
+});
 </script>
 
 <template>
-  <div class="home">
-    <Button variant="secondary" class="shadow-md mx-auto block" @click="getStarted">Get Started</Button>
+  <div class="flex flex-col items-center gap-5">
+    <div class="flex items-center space-x-4">
+      <Skeleton class="h-12 w-12 rounded-full" />
+      <div class="space-y-2">
+        <Skeleton class="h-4 w-[200px]" />
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div  class="flex flex-col space-y-3">
+      <Skeleton class="h-[125px] w-[250px] rounded-xl" />
+      <div class="space-y-2">
+        <Skeleton class="h-4 w-[250px]" />
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+    </div>
   </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 // See README.md for more for routing routing
 
 export default [
-  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'Home', description: "Welcome to the Home Page", icon: "Home" } },
+  { path: '/', component: () => import('./pages/Home.vue'), name: 'home', meta: { displayName: 'Getting started', description: "Preparing you're environment", icon: "Home" } },
   { path: '/setup', component: () => import('./pages/Setup.vue'), name: 'setup', meta: { displayName: 'Setup', description: "Fill out the form below to analyze your document.", icon: "UserCog" } },
   { path: '/contract-analysis', component: () => import('./pages/ContractAnalysis.vue'), name: 'contract-analysis', meta: { displayName: 'Contract Analysis', description: "Analyze your document for Fairness, Onerousness, & Enforceability", icon: "FileBarChart2" } },
   { path: '/legal-text', component: () => import('./pages/LegalText.vue'), name: 'legal-text', meta: { displayName: 'Legal Text', description: "", icon: "Gavel" } },
