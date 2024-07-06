6dd8458cab61c0820491533b6098044f7752c437
Altbeg <shmuelbisberg.acc@gmail.com>
2024-07-06 23:17:03 +0300

    navigate in google handlers

--- src/lib/doc-store.js
+++ src/lib/doc-store.js
@@ -1,6 +1,9 @@
 // Store for the document data and state
 
 import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 export const docStore = reactive({
   // data from "users/:id/documents/:docId" in firebase
@@ -14,24 +17,23 @@ export const docStore = reactive({
   getDoc() {
     google.script.run.withSuccessHandler(function (doc) {
       docStore.doc = doc;
-      console.log(doc);
+      if (!doc.configured) {
+        router.push({ name: "setup" });
+      } else {
+        router.push({ name: "contract-analysis" });
+      }
     }).withFailureHandler(function (error) {
-      console.error('Failed to get document', error);
+      console.error(error);
     }).getDocument();
   },
 
   // trigger `configDocument` on `main.gs` to save the document data
   saveDoc(conf) {
-    let isSuccessful = false;
-
     google.script.run.withSuccessHandler(function (newConf) {
       docStore.doc = newConf;
-      console.log(newConf);
-      isSuccessful = true;
+      router.push({ name: "contract-analysis" });
     }).withFailureHandler(function (error) {
-      console.error('Failed to save document', error);
+      console.error(error);
     }).configDocument(conf);
-
-    return isSuccessful;
   },
 });
